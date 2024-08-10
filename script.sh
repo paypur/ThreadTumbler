df613a64640c607d311d705b540e512aee10f10f
paypur <pieceofpaypur@gmail.com>
2024-08-10 14:24:54 -0700

    Added exit trap
    Took 42 minutes

--- script.sh
+++ script.sh
@@ -2,23 +2,35 @@
 
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
-core_num=0
-run_time_per_core=3600
+core_count=$(nproc)
+tumble_time=300
+index=0
 
-# physical cores have their thread #'s grouped in pairs
 # shellcheck disable=SC2207
+# physical cores have their thread #'s grouped in pairs
 arr=( $(lstopo-no-graphics | grep -E "\(P#[0-9]*\)" | sed s/'PU L#[0-9]* (P#'// | sed s/')'// | xargs) )
 
+stop() {
+    for proc in $(pgrep -f y-cruncher); do
+        kill $proc
+    done
+    exit $?
+}
+
+trap stop EXIT
 #y-cruncher stress -M:8g &
+
+
+# TODO: kill all process except for one to maybe reduce context switching
 
 # shellcheck disable=SC2159
 while [ 0 ]; do
     if [ "$(cat /sys/devices/system/cpu/smt/active)" -eq 1 ]; then
-        taskset -apc "${arr[core_num]},${arr[core_num+1]}" "$(pgrep -f /usr/lib/y-cruncher/Binaries/)"
-        core_num=$((core_num+2))
+        taskset -apc "${arr[$index]},${arr[$index+1]}" "$(pgrep -f /usr/lib/y-cruncher/Binaries/)"
+        index=$(((index+2)%core_count))
     else
-        taskset -apc $core_num "$(pgrep -f /usr/lib/y-cruncher/Binaries/)"
-        core_num=$((core_num+1))
+        taskset -apc $index "$(pgrep -f /usr/lib/y-cruncher/Binaries/)"
+        index=$(((index+1)%core_count))
     fi
-    sleep $run_time_per_core
+    sleep $tumble_time
 done
