cb35c34cf4ba5cc95162cf5e25830cc4c55c467e
paypur <pieceofpaypur@gmail.com>
2024-08-15 19:40:11 -0700

    Changed affinity reassigning logic
    Took 1 hour 54 minutes

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 core_count=$(nproc)
-tumble_time=300
 index=0
 
 # shellcheck disable=SC2207
@@ -12,28 +11,24 @@ arr=( $(lstopo-no-graphics | grep -E "\(P#[0-9]*\)" | sed s/'PU L#[0-9]* (P#'// 
 
 stop() {
     for proc in $(pgrep -f y-cruncher); do
-        kill $proc
+        kill "$proc"
     done
     exit $?
 }
 
 trap stop EXIT
 
-y-cruncher stress 2>&1 | tee outfile &
+y-cruncher stress -M:8g -D:5 2>&1 | tee logfile &
 
-# TODO: kill all process except for one to maybe reduce context switching
-
-if tail -f outfile | grep -Eq "Allocating Memory..."; then
-    echo "setting affinity"
-    # shellcheck disable=SC2159
-    while [ 0 ]; do
-       if [ "$(cat /sys/devices/system/cpu/smt/active)" -eq 1 ]; then
-           taskset -apc "${arr[$index]},${arr[$index+1]}" "$(pgrep -f /usr/lib/y-cruncher/Binaries/)"
-           index=$(((index+2)%core_count))
-       else
-           taskset -apc $index "$(pgrep -f /usr/lib/y-cruncher/Binaries/)"
-           index=$(((index+1)%core_count))
-       fi
-       sleep $tumble_time
-   done
-fi
+while [ 0 ]; do
+    if tail -f -n 1 logfile | grep -Eq "Iteration:"; then
+        # shellcheck disable=SC2159
+        if [ "$(cat /sys/devices/system/cpu/smt/active)" -eq 1 ]; then
+            taskset -apc "${arr[$index]},${arr[$index+1]}" "$(pgrep -f /usr/lib/y-cruncher/Binaries/)" &> /dev/null
+            index=$(((index+2)%core_count))
+        else
+            taskset -apc $index "$(pgrep -f /usr/lib/y-cruncher/Binaries/)" &> /dev/null
+            index=$(((index+1)%core_count))
+        fi
+    fi
+done
